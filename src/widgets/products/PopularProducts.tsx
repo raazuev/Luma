95976f2816ebb7ccb6c99224ea7294686ee1fa9b
Razuev Anton <Raazuev@gmail.com>
2025-02-10 00:22:37 +0500

    Added API for popular products

--- src/widgets/products/PopularProducts.tsx
+++ src/widgets/products/PopularProducts.tsx
@@ -1,7 +1,27 @@
-import { POPULAR } from "./store/PopularMock";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/shared/hooks/reduxHooks";
+import { fetchProducts } from "@/entities/products/store/ProductsSlice";
+import { Spinner } from "@/shared/ui/spinner/Spinner";
 import styles from "./styles/PopularProducts.module.scss";
 
 export const PopularProducts: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { products, loading, error } = useAppSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (loading)
+    return (
+      <p>
+        <Spinner />
+      </p>
+    );
+  if (error) return <p>{error}</p>;
+
   return (
     <div className={styles.container}>
       <div className={styles.popular}>
@@ -9,15 +29,15 @@ export const PopularProducts: React.FC = () => {
           <h2 className={styles.h2}>Популярные товары</h2>
         </div>
         <ul className={styles.popular__item}>
-          {POPULAR.map((popular) => (
+          {products.map((popular) => (
             <li className={styles.li} key={popular.id}>
               <img
                 className={styles.img}
-                src={popular.img}
+                src={popular.imageUrl}
                 alt={popular.title}
               ></img>
               <h3 className={styles.h3}>{popular.title}</h3>
-              <span className={styles.span}>{popular.price}</span>
+              <span className={styles.span}>{popular.price}$</span>
             </li>
           ))}
         </ul>
